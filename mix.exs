bdfe5b170329a7673451b52d5177ed396456e7d6
Matt Bone <thatmattbone@gmail.com>
2025-08-05 23:17:27 -0500

    update mix.exs and add lock file

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule BoundedMapBuffer.MixProject do
     [
       app: :bounded_map_buffer,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       package: package(),
       deps: deps(),
